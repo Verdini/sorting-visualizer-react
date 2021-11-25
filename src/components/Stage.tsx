8edf348cca8f0169e4bbe7b88c82df9146d8b8cd
Marcos <marcos.verdini@mte-thomson.com.br>
2021-11-25 17:27:51 -0300

    Drawing array elements on screen

--- src/components/Stage.tsx
+++ src/components/Stage.tsx
@@ -13,7 +13,7 @@ class Stage extends React.Component<IProps, IState> {
   constructor(props: IProps) {
     super(props);
     this.state = {
-        array: [1, 2, 3, 4]
+        array: Array.from({length: 40}, () => ~~(1 + Math.random() * 100))
     };
   }
 
@@ -24,8 +24,13 @@ class Stage extends React.Component<IProps, IState> {
   render() {
     return(
         <div id="container">
-            {this.state.array.map( (el) => 
-                <div>{el}</div>
+            {this.state.array.map( (el: any, index) => 
+                <div 
+                  className="arrayElement" 
+                  key={index}
+                  style={ { height: `${el*5}px` } } >
+                  {el}
+                </div>
             )}
         </div>
     );
